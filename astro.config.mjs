03211e7e20d401e41ae3ece6f2e033acd7cdda2d
Oriza <nixing.zone@gmail.com>
2023-07-15 15:54:03 +0000

    setup github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://docs.nix.zone',
   integrations: [
     starlight({
       title: "Nix Zone!!",
