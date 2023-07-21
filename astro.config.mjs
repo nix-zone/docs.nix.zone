61b5c778c1eca0a32e0d9a2198b2392b342e56c4
OshDubh <60195526+OshDubh@users.noreply.github.com>
2023-07-21 21:46:03 +0000

    astro added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,35 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.nix.zone',
-  integrations: [
-    starlight({
-      title: "Nix Zone!!",
-      social: {
-        github: "https://github.com/Nix-zone/Docs.nix.zone",
-      },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", link: "/guides/example/" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
-
+  integrations: [starlight({
+    title: "Nix Zone!!",
+    social: {
+      github: "https://github.com/Nix-zone/Docs.nix.zone"
+    },
+    sidebar: [{
+      label: "Guides",
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: "Example Guide",
+        link: "/guides/example/"
+      }]
+    }, {
+      label: "Reference",
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  }), tailwind()],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: "astro/assets/services/sharp" } },
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp"
+    }
+  }
 });
